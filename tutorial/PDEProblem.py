5e2de9383a142384c3b16b97d6c5f4c4f13cd537
karanh97 <karan.hiranandani@utexas.edu>
2023-04-03 08:54:43 -0500

    Pushing test file with major bug fixes

---
+++ tutorial/PDEProblem.py
@@ -0,0 +1,363 @@
+# Copyright (c) 2016-2018, The University of Texas at Austin
+# & University of California--Merced.
+# Copyright (c) 2019-2020, The University of Texas at Austin
+# University of California--Merced, Washington University in St. Louis.
+#
+# All Rights reserved.
+# See file COPYRIGHT for details.
+#
+# This file is part of the hIPPYlib library. For more information and source code
+# availability see https://hippylib.github.io.
+#
+# hIPPYlib is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License (as published by the Free
+# Software Foundation) version 2.0 dated June 1991.
+
+# import dolfin as dl
+import firedrake as fd
+import ufl
+import numpy as np
+import petsc4py
+from variables import STATE, PARAMETER, ADJOINT
+from linalg import Transpose, matVecMult
+from linSolvers import CreateSolver
+from vector2function import vector2Function
+
+class PDEProblem(object):
+    """ Consider the PDE problem:
+        Given :math:`m`, find :math:`u` such that 
+        
+            .. math:: F(u, m, p) = ( f(u, m), p) = 0, \\quad \\forall p.
+        
+        Here :math:`F` is linear in :math:`p`, but it may be non linear in :math:`u` and :math:`m`.
+    """
+
+    def generate_state(self):
+        """ Return a vector in the shape of the state. """
+        raise NotImplementedError("Child class should implement method generate_state")
+
+    def generate_parameter(self):
+        """ Return a vector in the shape of the parameter. """
+        raise NotImplementedError("Child class should implement method generate_parameter")
+
+    def init_parameter(self, m):
+        """ Initialize the parameter. """
+        raise NotImplementedError("Child class should implement method init_parameter")
+
+    def solveFwd(self, state, x):
+        """ Solve the possibly nonlinear forward problem:
+        Given :math:`m`, find :math:`u` such that
+
+            .. math:: \\delta_p F(u, m, p;\\hat{p}) = 0, \\quad \\forall \\hat{p}.
+        """
+        raise NotImplementedError("Child class should implement method solveFwd")
+
+    def solveAdj(self, adj, x, adj_rhs):
+        """ Solve the linear adjoint problem: 
+            Given :math:`m`, :math:`u`; find :math:`p` such that
+            
+                .. math:: \\delta_u F(u, m, p;\\hat{u}) = 0, \\quad \\forall \\hat{u}.
+        """
+        raise NotImplementedError("Child class should implement method solveAdj")
+
+    def evalGradientParameter(self, x, out):
+        """Given :math:`u, m, p`; evaluate :math:`\\delta_m F(u, m, p; \\hat{m}),\\, \\forall \\hat{m}.` """
+        raise NotImplementedError("Child class should implement method evalGradientParameter")
+ 
+    def setLinearizationPoint(self, x, gauss_newton_approx):
+
+        """ Set the values of the state and parameter
+            for the incremental forward and adjoint solvers. 
+            Set whether Gauss Newton approximation of
+            the Hessian should be used."""
+        raise NotImplementedError("Child class should implement method setLinearizationPoint")
+      
+    def solveIncremental(self, out, rhs, is_adj):
+        """ If :code:`is_adj = False`:
+
+            Solve the forward incremental system:
+            Given :math:`u, m`, find :math:`\\tilde{u}` such that
+
+            .. math::
+                \\delta_{pu} F(u, m, p; \\hat{p}, \\tilde{u}) = \\mbox{rhs}, \\quad \\forall \\hat{p}.
+            
+            If :code:`is_adj = True`:
+            
+            Solve the adjoint incremental system:
+            Given :math:`u, m`, find :math:`\\tilde{p}` such that
+
+            .. math::
+                \\delta_{up} F(u, m, p; \\hat{u}, \\tilde{p}) = \\mbox{rhs}, \\quad \\forall \\hat{u}.
+        """
+        raise NotImplementedError("Child class should implement method solveIncremental")
+
+    def apply_ij(self, i, j, dir, out):
+        """
+            Given :math:`u, m, p`; compute 
+            :math:`\\delta_{ij} F(u, m, p; \\hat{i}, \\tilde{j})` in the direction :math:`\\tilde{j} =` :code:`dir`, 
+            :math:`\\forall \\hat{i}.`
+        """
+        raise NotImplementedError("Child class should implement method apply_ij")
+        
+    def apply_ijk(self, i, j, k, x, jdir, kdir, out):
+        """
+            Given :code:`x = [u,a,p]`; compute
+            :math:`\\delta_{ijk} F(u,a,p; \\hat{i}, \\tilde{j}, \\tilde{k})`
+            in the direction :math:`(\\tilde{j},\\tilde{k}) = (`:code:`jdir,kdir`), :math:`\\forall \\hat{i}.`
+        """
+        raise NotImplementedError("Child class should implement apply_ijk")
+
+class PDEVariationalProblem(PDEProblem):
+    def __init__(self, Vh, varf_handler, bc, bc0, is_fwd_linear=False):
+        self.Vh = Vh
+        self.varf_handler = varf_handler
+        if type(bc) is fd.bcs.DirichletBC:
+            self.bc = [bc]
+        else:
+            self.bc = bc
+        if type(bc0) is fd.bcs.DirichletBC:
+            self.bc0 = [bc0]
+        else:
+            self.bc0 = bc0
+        
+        self.A = None
+        self.At = None
+        self.C = None
+        self.Wmu = None
+        self.Wmm = None
+        self.Wuu = None
+        self.solver = None
+        self.solver_fwd_inc = None
+        self.solver_adj_inc = None
+        
+        self.is_fwd_linear = is_fwd_linear
+        self.n_calls = {"forward": 0,
+                        "adjoint": 0,
+                        "incremental_forward": 0,
+                        "incremental_adjoint": 0}
+
+    def generate_state(self):
+        """ Return a vector in the shape of the state. """
+        return fd.Function(self.Vh[STATE]).vector()
+    
+    def generate_parameter(self):
+        """ Return a vector in the shape of the parameter. """
+        return fd.Function(self.Vh[PARAMETER]).vector()
+    
+    def init_parameter(self, m):  # CROSSCHECK
+        """ Initialize the parameter. """
+        dummy = self.generate_parameter()
+        m.init(dummy.comm, dummy.local_range())
+    
+    def solveFwd(self, state, x):
+        """ Solve the possibly nonlinear forward problem:
+        Given :math:`m`, find :math:`u` such that
+        
+            .. math:: \\delta_p F(u, m, p;\\hat{p}) = 0,\\quad \\forall \\hat{p}."""
+        # Firedrake solver requires an operator A to be defined for a solver
+        if self.A is None:      # confirm if the index for self.Vh is correct
+            self.A = fd.assemble(fd.inner(fd.TestFunction(self.Vh[STATE]),
+                                          fd.TrialFunction(self.Vh[STATE])) * fd.dx)
+        self.n_calls["forward"] += 1
+        if self.solver is None:
+            self.solver = self._createLUSolver()
+        if self.is_fwd_linear:
+            u = fd.TrialFunction(self.Vh[STATE])
+            m = vector2Function(x[PARAMETER].vector(), self.Vh[PARAMETER])
+            p = fd.TestFunction(self.Vh[ADJOINT])
+            res_form = self.varf_handler(u, m, p)
+            A_form = ufl.lhs(res_form)
+            b_form = ufl.rhs(res_form)
+            self.A = fd.assemble(A_form, bcs=self.bc)
+            b = fd.assemble(b_form, bcs=self.bc).vector()
+            self.solver = fd.LinearSolver(self.A)
+            self.solver.solve(state, b)
+        else:
+            u = vector2Function(x[STATE], self.Vh[STATE])
+            m = vector2Function(x[PARAMETER], self.Vh[PARAMETER])
+            p = fd.TestFunction(self.Vh[ADJOINT])
+            res_form = self.varf_handler(u, m, p)
+            fd.solve(res_form == 0, u, bcs=self.bc)
+            state.vector().assign(0.0)
+            state.axpy(1., u.vector())    # axpy in fd gives compilation error
+            # state.vector().set_local(u.vector().get_local())
+        x[STATE].set_local(np.flip(x[STATE].get_local()))
+
+    def solveAdj(self, adj, x, adj_rhs):
+        """ Solve the linear adjoint problem: 
+            Given :math:`m, u`; find :math:`p` such that
+            
+                .. math:: \\delta_u F(u, m, p;\\hat{u}) = 0, \\quad \\forall \\hat{u}.
+        """
+        self.n_calls["adjoint"] += 1
+        if self.solver is None:
+            self.solver = self._createLUSolver()
+            
+        u = vector2Function(x[STATE], self.Vh[STATE])
+        m = vector2Function(x[PARAMETER], self.Vh[PARAMETER])
+        p = fd.Function(self.Vh[ADJOINT])
+        du = fd.TestFunction(self.Vh[STATE])
+        dp = fd.TrialFunction(self.Vh[ADJOINT])
+        varf = self.varf_handler(u, m, p)
+        adj_form = fd.derivative(fd.derivative(varf, u, du), p, dp)
+        Aadj = fd.assemble(adj_form, bcs=self.bc0)
+        # dummy = fd.assemble(fd.inner(u , du) * fd.dx, bcs=self.bc0)
+        self.solver = fd.LinearSolver(Aadj)
+        self.solver.solve(adj, adj_rhs)
+     
+    def evalGradientParameter(self, x, out):
+        """Given :math:`u, m, p`; evaluate :math:`\\delta_m F(u, m, p; \\hat{m}),\\, \\forall \\hat{m}.` """
+        u = vector2Function(x[STATE], self.Vh[STATE])
+        m = vector2Function(x[PARAMETER], self.Vh[PARAMETER])
+        p = vector2Function(x[ADJOINT], self.Vh[ADJOINT])
+        dm = fd.TestFunction(self.Vh[PARAMETER])
+        res_form = self.varf_handler(u, m, p)
+        out.vector().assign(0.0)
+        fd.assemble(fd.derivative(res_form, m, dm), tensor=out)
+
+    def setLinearizationPoint(self, x, gauss_newton_approx):
+        """ Set the values of the state and parameter
+            for the incremental forward and adjoint solvers. """
+
+        x_fun = [vector2Function(x[i], self.Vh[i]) for i in range(3)]
+
+        f_form = self.varf_handler(*x_fun)
+
+        g_form = [None, None, None]
+        for i in range(3):
+            g_form[i] = fd.derivative(f_form, x_fun[i])
+            
+        self.A = fd.assemble(fd.derivative(g_form[ADJOINT], x_fun[STATE]), bcs=self.bc0)
+        self.At = fd.assemble(fd.derivative(g_form[STATE], x_fun[ADJOINT]), bcs=self.bc0)
+        self.C = fd.assemble(fd.derivative(g_form[ADJOINT], x_fun[PARAMETER]))
+        # self.Ct = fd.assemble(fd.derivative(g_form[PARAMETER], x_fun[ADJOINT]))
+        # [bc.zero(self.C) for bc in self.bc0]
+        # for bc in self.bc0:
+        #     bc.homogenize()
+        #     bc.apply(self.C)
+
+        if self.solver_fwd_inc is None:
+            self.solver_fwd_inc = self._createLUSolver()
+            self.solver_adj_inc = self._createLUSolver()
+        
+        self.solver_fwd_inc = fd.LinearSolver(self.A)
+        self.solver_adj_inc = fd.LinearSolver(self.At)
+
+        if gauss_newton_approx:
+            self.Wuu = None
+            self.Wmu = None
+            self.Wmm = None
+        else:
+            self.Wuu = fd.assemble(fd.derivative(g_form[STATE], x_fun[STATE]))
+            # for bc in self.bc0:
+            #     bc.homogenize()
+            #     bc.apply(self.Wuu)
+            # Wuu_t = Transpose(self.Wuu)
+            # for bc in self.bc0:
+            #     bc.homogenize()
+            #     bc.apply(Wuu_t)
+            # [bc.zero(Wuu_t) for bc in self.bc0]
+            # self.Wuu = Transpose(Wuu_t)
+            self.Wmu = fd.assemble(fd.derivative(g_form[PARAMETER],x_fun[STATE]))
+            # print(self.Wmu.M.handle.size)
+            # Wmu_t = Transpose(self.Wmu)
+            # for bc in self.bc0:
+            #     bc.homogenize()
+            #     bc.apply(Wmu_t)
+            # [bc.zero(Wmu_t) for bc in self.bc0]
+            # self.Wmu = Transpose(Wmu_t)
+            self.Wmm = fd.assemble(fd.derivative(g_form[PARAMETER], x_fun[PARAMETER]))
+        
+    def solveIncremental(self, out, rhs, is_adj):
+        """ If :code:`is_adj == False`:
+
+            Solve the forward incremental system:
+            Given :math:`u, m`, find :math:`\\tilde{u}` such that
+            
+                .. math:: \\delta_{pu} F(u, m, p; \\hat{p}, \\tilde{u}) = \\mbox{rhs},\\quad \\forall \\hat{p}.
+            
+            If :code:`is_adj == True`:
+
+            Solve the adjoint incremental system:
+            Given :math:`u, m`, find :math:`\\tilde{p}` such that
+            
+                .. math:: \\delta_{up} F(u, m, p; \\hat{u}, \\tilde{p}) = \\mbox{rhs},\\quad \\forall \\hat{u}.
+        """
+        if is_adj:
+            self.n_calls["incremental_adjoint"] += 1
+            self.solver_adj_inc.solve(out, rhs)
+        else:
+            self.n_calls["incremental_forward"] += 1
+            self.solver_fwd_inc.solve(out, rhs)
+    
+    def apply_ij(self, i, j, dir, out):
+        """
+            Given :math:`u, m, p`; compute 
+            :math:`\\delta_{ij} F(u, m, p; \\hat{i}, \\tilde{j})` in the direction :math:`\\tilde{j} =` :code:`dir`,
+            :math:`\\forall \\hat{i}`.
+        """
+        # print(self.Wmu.M.handle.size)
+        KKT = {}
+        KKT[STATE, STATE] = self.Wuu
+        KKT[PARAMETER, STATE] = self.Wmu
+        KKT[PARAMETER, PARAMETER] = self.Wmm
+        KKT[ADJOINT, STATE] = self.A
+        KKT[ADJOINT, PARAMETER] = self.C
+
+        if j == STATE or j == ADJOINT:
+            fun = vector2Function(dir, dir.function_space())
+            for bc in self.bc0:
+                bc.apply(fun)
+            dir = fun.vector()
+
+        if i >= j:
+            if KKT[i, j] is None:
+                out.vector().assign(0.0)
+            else:
+                out = matVecMult(KKT[i, j], dir, out)
+                # fun = vector2Function(out, out.function_space())
+                # [bc.apply(fun) for bc in self.bc0]
+                # out = fun.vector()
+                # out = fun.vector()
+        else:
+            if KKT[j, i] is None:
+                out.vector().assign(0.0)
+            else:
+                KKT[i, j] = Transpose(KKT[j, i])
+                print(id(KKT[j, i]), id(KKT[i, j]), id(self.Wmu))
+                out = matVecMult(KKT[i, j], dir, out)
+                # fun = vector2Function(out, out.function_space())
+                # [bc.apply(fun) for bc in self.bc0]
+                # out = fun.vector()
+        if i == STATE or i == ADJOINT:
+            fun = vector2Function(out, out.function_space())
+            [bc.apply(fun) for bc in self.bc0]
+            out = fun.vector()
+
+
+    def apply_ijk(self, i, j, k, x, jdir, kdir, out):
+        x_fun = [vector2Function(x[ii], self.Vh[ii]) for ii in range(3)]
+        idir_fun = fd.TestFunction(self.Vh[i])
+        jdir_fun = vector2Function(jdir, self.Vh[j])
+        kdir_fun = vector2Function(kdir, self.Vh[k])
+
+        res_form = self.varf_handler(*x_fun)
+        form = fd.derivative(
+               fd.derivative(
+               fd.derivative(res_form, x_fun[i], idir_fun),
+               x_fun[j], jdir_fun),
+               x_fun[k], kdir_fun)
+
+        out.vector().assign(0.0)
+        fd.assemble(form, tensor=out)
+
+        if i in [STATE, ADJOINT]:
+            fun = vector2Function(out, out.function_space())
+            for bc in self.bc0:
+                bc.apply(fun)
+            out = fun.vector()
+
+    def _createLUSolver(self):
+        # Can be used to create different solvers by specifying ksp and pre
+        solver = CreateSolver(self.A, self.Vh[STATE].mesh().comm)
+        return solver
--- tutorial/test.py
+++ tutorial/test.py
@@ -1,11 +1,12 @@
 # loading modules
+import sys
+import os
+import math
 import firedrake as fd
 import ufl
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
-import os
-import math
+import nb
 
 sys.path.append(os.environ.get('HIPPYFIRE_BASE_DIR', '../') )
 from hippyfire import *
@@ -13,16 +14,11 @@ from variables import STATE, PARAMETER, ADJOINT
 from PDEProblem import PDEVariationalProblem
 from rand import randomGen
 from prior import BiLaplacianPrior
+from misfit import ContinuousStateObservation
+from linalg import matVecMult
+from model import Model
+from modelVerify import modelVerify
 
-# Generate true parameter
-def true_model(prior):
-    noise = dl.Vector()
-    prior.init_vector(noise,"noise")
-    parRandom.normal(1., noise)
-    mtrue = dl.Vector()
-    prior.init_vector(mtrue, 0)
-    # prior.sample(noise,mtrue)   # sample not defined in any prior's child classes
-    return mtrue
 
 # Set up mesh and finite element spaces
 ndim = 2
@@ -47,9 +43,11 @@ bc0 = fd.DirichletBC(Vh[STATE], u_bdr0, [3, 4])
 
 f = fd.Constant(0.0)
 
+
 def pde_varf(u, m, p):
     return ufl.exp(m) * ufl.inner(ufl.grad(u), ufl.grad(p)) * ufl.dx - f * p * ufl.dx
-pde = PDEVariationalProblem(Vh, pde_varf, bc, bc0, is_fwd_linear=True)
+
+pde = PDEVariationalProblem(Vh, pde_varf, bc, bc0, is_fwd_linear=False)
 
 # Set up prior
 gamma = .1
@@ -57,18 +55,42 @@ delta = .5
 
 theta0 = 2.
 theta1 = .5
-alpha  = math.pi / 4
-tup = (theta0, theta1, alpha)
-anis_diff = fd.Constant(tup)
-prior = BiLaplacianPrior(Vh[PARAMETER], gamma, delta, anis_diff, robin_bc=True)
+alpha = math.pi / 4
+# tup = (theta0, theta1, alpha)
+# anis_diff = fd.Constant(tup)
+pr = BiLaplacianPrior(Vh[PARAMETER], gamma, delta, robin_bc=True)
+mtrue = (randomGen(Vh[PARAMETER])).vector()
+objs = [fd.Function(Vh[PARAMETER], mtrue), fd.Function(Vh[PARAMETER], pr.mean)]
+# plt.plot(objs)
 
 # Set up misfit
 ntargets = 50
 rel_noise = 0.01
 
 #Targets only on the bottom
-targets_x = np.random.uniform(0.1,0.9, [ntargets] )
-targets_y = np.random.uniform(0.1,0.5, [ntargets] )
-targets = np.zeros([ntargets, ndim])
-targets[:,0] = targets_x
-targets[:,1] = targets_y
+# targets_x = np.random.uniform(0.1,0.9, [ntargets] )
+# targets_y = np.random.uniform(0.1,0.5, [ntargets] )
+# targets = np.zeros([ntargets, ndim])
+# targets[:,0] = targets_x
+# targets[:,1] = targets_y
+# print( "Number of observation points: {0}".format(ntargets) )
+
+misfit = ContinuousStateObservation(Vh[STATE], ufl.dx, bcs=[bc0])
+utrue = pde.generate_state()
+x = [utrue, mtrue, None]
+pde.solveFwd(x[STATE], x)
+
+# print(misfit.W.M.handle.size)
+misfit.d = matVecMult(misfit.W, x[STATE], misfit.d)
+# print(misfit.d.get_local())
+vmax = max( utrue.get_local().max(), misfit.d.get_local().max() )
+vmin = min( utrue.get_local().min(), misfit.d.get_local().min() )
+print(vmax, vmin)
+# plt.figure(figsize=(15,5))
+# nb.plot(fd.Function(Vh[STATE], utrue), mytitle="True State", subplot_loc=121, vmin=vmin, vmax=vmax)
+# nb.plot_pts(targets, misfit.d, mytitle="Observations", subplot_loc=122, vmin=vmin, vmax=vmax)
+# plt.show()
+model = Model(pde, pr, misfit)
+x = fd.SpatialCoordinate(mesh)
+m0 = fd.interpolate(fd.sin(x[0]), Vh[PARAMETER])
+_ = modelVerify(model, m0.vector())
