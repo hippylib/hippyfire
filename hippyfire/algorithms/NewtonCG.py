5179a298da8a4db4af90edf3e7b6fd19d648035b
karanh97 <karan.hiranandani@utexas.edu>
2023-04-05 01:48:58 -0500

    Added methods LS_ParameterList(), TR_ParameterList(), ReducedSpaceNewtonCG_ParameterList()

--- hippyfire/algorithms/NewtonCG.py
+++ hippyfire/algorithms/NewtonCG.py
@@ -17,4 +17,45 @@ import math
 from utils.parameterList import ParameterList
 from modeling.reducedHessian import ReducedHessian
 from modeling.variables import STATE, PARAMETER, ADJOINT
-from cgsolverSteihaug import CGSolverSteihaug
+from algorithms.cgsolverSteihaug import CGSolverSteihaug
+
+def LS_ParameterList():
+    """
+    Generate a ParameterList for line search globalization.
+    type: :code:`LS_ParameterList().showMe()` for default values and their descriptions
+    """
+    parameters = {}
+    parameters["c_armijo"]              = [1e-4, "Armijo constant for sufficient reduction"]
+    parameters["max_backtracking_iter"] = [10, "Maximum number of backtracking iterations"]
+
+    return ParameterList(parameters)
+
+def TR_ParameterList():
+    """
+    Generate a ParameterList for Trust Region globalization.
+    type: :code:`RT_ParameterList().showMe()` for default values and their descriptions
+    """
+    parameters = {}
+    parameters["eta"] = [0.05, "Reject step if (actual reduction)/(predicted reduction) < eta"]
+
+    return ParameterList(parameters)
+
+def ReducedSpaceNewtonCG_ParameterList():
+    """
+    Generate a ParameterList for ReducedSpaceNewtonCG.
+    type: :code:`ReducedSpaceNewtonCG_ParameterList().showMe()` for default values and their descriptions
+    """
+    parameters = {}
+    parameters["rel_tolerance"]         = [1e-6, "we converge when sqrt(g,g)/sqrt(g_0,g_0) <= rel_tolerance"]
+    parameters["abs_tolerance"]         = [1e-12, "we converge when sqrt(g,g) <= abs_tolerance"]
+    parameters["gdm_tolerance"]         = [1e-18, "we converge when (g,dm) <= gdm_tolerance"]
+    parameters["max_iter"]              = [20, "maximum number of iterations"]
+    parameters["globalization"]         = ["LS", "Globalization technique: line search (LS)  or trust region (TR)"]
+    parameters["print_level"]           = [0, "Control verbosity of printing screen"]
+    parameters["GN_iter"]               = [5, "Number of Gauss Newton iterations before switching to Newton"]
+    parameters["cg_coarse_tolerance"]   = [.5, "Coarsest tolerance for the CG method (Eisenstat-Walker)"]
+    parameters["cg_max_iter"]           = [100, "Maximum CG iterations"]
+    parameters["LS"]                    = [LS_ParameterList(), "Sublist containing LS globalization parameters"]
+    parameters["TR"]                    = [TR_ParameterList(), "Sublist containing TR globalization parameters"]
+
+    return ParameterList(parameters)
