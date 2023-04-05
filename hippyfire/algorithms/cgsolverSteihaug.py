f4080ae7fb015aaf5534419650cf11f2d6bfb7ae
karanh97 <karan.hiranandani@utexas.edu>
2023-04-05 01:06:09 -0500

    Completed porting of cgsolverSteihaug

--- hippyfire/algorithms/cgsolverSteihaug.py
+++ hippyfire/algorithms/cgsolverSteihaug.py
@@ -15,6 +15,7 @@
 
 import firedrake as fd
 from utils.parameterList import ParameterList
+from algorithms.linalg import matVecMult
 import math
 
 
@@ -71,7 +72,7 @@ class CGSolverSteihaug:
               "Reached a negative direction",
               "Reached a trust region boundary"
               ]
-    def __init__(self, parameters=CGSolverSteihaug_ParameterList(), comm=fd.COMM_WORLD, Vh):
+    def __init__(self, Vh, parameters=CGSolverSteihaug_ParameterList(), comm=fd.COMM_WORLD):
         # using Vh instead of comm to construct vectors because Firedrake cannot construct
         # vectors with comm yet. Vh is obtained by extracting the function space of the
         # BilaplcianR attribute of the prior
@@ -125,3 +126,165 @@ class CGSolverSteihaug:
         v1, u1 = self.B_op.form.arguments()
         # self.B_op.init_vector(self.Bx,0)
         self.Bx = fd.Function(u1.function_space()).vector()
+
+    def update_x_without_TR(self, x, alpha, d):
+        x.axpy(alpha, d)
+        return False
+
+    def update_x_with_TR(self, x, alpha, d):
+        x_bk = x.copy()
+        x.axpy(alpha, d)
+        self.Bx.assign(0.0)
+        # self.B_op.mult(x, self.Bx)
+        matVecMult(self.B_op, x, self.Bx)
+        x_Bnorm2 = self.Bx.inner(x)
+
+        if x_Bnorm2 < self.TR_radius_2:
+            return  False
+        else:
+            # Move point to boundary of trust region
+            self.Bx.assign(0.0)
+            # self.B_op.mult(x_bk, self.Bx)
+            matVecMult(self.B_op, x_bk, self.Bx)
+            x_Bnorm2 = self.Bx.inner(x_bk)
+            Bd = self.d.copy()
+            Bd.assign(0.0)
+            # self.B_op.mult(self.d,Bd)
+            matVecMult(self.B_op, self.d, Bd)
+            d_Bnorm2 = Bd.inner(d)
+            d_Bx = Bd.inner(x_bk)
+            a_tau = alpha * alpha * d_Bnorm2
+            b_tau_half = alpha * d_Bx
+            c_tau = x_Bnorm2 - self.TR_radius_2
+            # Solve quadratic for :code:`tau`
+            tau = (-b_tau_half + math.sqrt(b_tau_half * b_tau_half - a_tau * c_tau)) / a_tau
+            x.assign(0.0)
+            x.axpy(1, x_bk)
+            x.axpy(tau * alpha, d)
+            return True
+
+    def solve(self, x, b):
+        """
+        Solve the linear system :math:`Ax = b`
+        """
+        self.iter = 0
+        self.converged = False
+        self.reasonid = 0
+
+        betanom = 0.0
+        alpha = 0.0
+        beta = 0.0
+
+        if self.parameters["zero_initial_guess"]:
+            self.r.assign(0.0)
+            self.r.axpy(1.0, b)
+            x.assign(0.0)
+        else:
+            assert self.TR_radius_2==None
+            # self.A.mult(x,self.r)
+            matVecMult(self.A, x, self.r)
+            self.r *= -1.0
+            self.r.axpy(1.0, b)
+
+        self.z.assign(0.0)
+        self.B_solver.solve(self.z, self.r) #z = B^-1 r
+
+        self.d.assign(0.0)
+        self.d.axpy(1., self.z); #d = z
+
+        nom0 = self.d.inner(self.r)
+        nom = nom0
+
+        if self.parameters["print_level"] == 1:
+            print(" Iteration : ", 0, " (B r, r) = ", nom)
+
+        rtol2 = nom * self.parameters["rel_tolerance"] * self.parameters["rel_tolerance"]
+        atol2 = self.parameters["abs_tolerance"] * self.parameters["abs_tolerance"]
+        r0 = max(rtol2, atol2)
+
+        if nom <= r0:
+            self.converged = True
+            self.reasonid = 1
+            self.final_norm = math.sqrt(nom)
+            if (self.parameters["print_level"] >= 0):
+                print(self.reason[self.reasonid])
+                print("Converged in ", self.iter, " iterations with final norm", self.final_norm)
+            return
+
+        # self.A.mult(self.d, self.Ad)
+        matVecMult(self.A, self.d, self.Ad)
+        den = self.Ad.inner(self.d)
+
+        if den <= 0.0:
+            self.converged = True
+            self.reasonid = 2
+            x.axpy(1., self.d)
+            self.r.axpy(-1., self.Ad)
+            self.B_solver.solve(self.z, self.r)
+            nom = self.r.inner(self.z)
+            self.final_norm = math.sqrt(nom)
+            if (self.parameters["print_level"] >= 0):
+                print(self.reason[self.reasonid])
+                print("Converged in ", self.iter, " iterations with final norm", self.final_norm)
+            return
+
+        # start iteration
+        self.iter = 1
+        while True:
+            alpha = nom / den
+            TrustBool = self.update_x(x, alpha, self.d)   # x = x + alpha d
+            if TrustBool:
+                self.converged = True
+                self.reasonid = 3
+                self.final_norm = math.sqrt(betanom)
+                if (self.parameters["print_level"] >= 0):
+                    print(self.reason[self.reasonid])
+                    print("Converged in ", self.iter, " iterations with final norm ", self.final_norm)
+                break
+
+            self.r.axpy(-alpha, self.Ad)  # r = r - alpha A d
+
+            self.B_solver.solve(self.z, self.r)     # z = B^-1 r
+            betanom = self.r.inner(self.z)
+
+            if self.parameters["print_level"] == 1:
+                print(" Iteration : ", self.iter, " (B r, r) = ", betanom)
+
+            if betanom < r0:
+                self.converged = True
+                self.reasonid = 1
+                self.final_norm = math.sqrt(betanom)
+                if (self.parameters["print_level"] >= 0):
+                    print(self.reason[self.reasonid])
+                    print("Converged in ", self.iter, " iterations with final norm ", self.final_norm)
+                break
+
+            self.iter += 1
+            if self.iter > self.parameters["max_iter"]:
+                self.converged = False
+                self.reasonid = 0
+                self.final_norm = math.sqrt(betanom)
+                if (self.parameters["print_level"] >= 0):
+                    print(self.reason[self.reasonid])
+                    print("Not Converged. Final residual norm ", self.final_norm )
+                break
+
+            beta = betanom / nom
+            self.d *= beta
+            self.d.axpy(1., self.z)  # d = z + beta d
+
+            # self.A.mult(self.d,self.Ad)
+            matVecMult(self.A, self.d, self.Ad)
+
+            den = self.d.inner(self.Ad)
+
+            if den <= 0.0:
+                self.converged = True
+                self.reasonid = 2
+                self.final_norm = math.sqrt(nom)
+                if (self.parameters["print_level"] >= 0):
+                    print(self.reason[self.reasonid] )
+                    print("Converged in ", self.iter, " iterations with final norm ", self.final_norm )
+                break
+
+            nom = betanom
